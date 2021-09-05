1994b71ec33095a133cfed3519cabc9ce9165dad
dmitry solodov <dsolodov@avito.ru>
2021-09-05 22:04:54 +0300

    Решение задач пятого модуля

--- 5-module/2-task/index.js
+++ 5-module/2-task/index.js
@@ -1,3 +1,13 @@
 function toggleText() {
-  // ваш код...
+  
+  const btn = document.querySelector('.toggle-text-button');
+  const textBlock = document.querySelector('#text');
+
+  btn.addEventListener('click', () => {
+    if ( textBlock.hidden ) {
+      textBlock.hidden = false;
+    } else 
+    textBlock.hidden = true;
+  });
+
 }
